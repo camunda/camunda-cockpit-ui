d737f5bb5411503a32a595a46040dd3d8d93b472
Jenkins <jenkins@camunda.com>
2014-12-05 14:15:09 +0100

    chore(release): Prepare next development version: 7.2.2-SNAPSHOT

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,12 +28,6 @@ module.exports = function(grunt) {
 
     pkg:              pkg,
 
-    bower:            require('camunda-commons-ui/grunt/config/bower')(config),
-
-    jasmine_node:     require('camunda-commons-ui/grunt/config/jasmine_node')(config),
-
-    karma:            require('camunda-commons-ui/grunt/config/karma')(config),
-
     requirejs:        require('./grunt/config/requirejs')(config),
 
     less:             require('camunda-commons-ui/grunt/config/less')(config),
@@ -47,8 +41,6 @@ module.exports = function(grunt) {
     jsdoc:            require('camunda-commons-ui/grunt/config/jsdoc')(config),
 
     jshint:           require('camunda-commons-ui/grunt/config/jshint')(config),
-
-    changelog:        require('camunda-commons-ui/grunt/config/changelog')(config),
 
     clean:            require('camunda-commons-ui/grunt/config/clean')(config)
   });
@@ -123,7 +115,6 @@ module.exports = function(grunt) {
       'clean',
 //       'jshint',
 //       'jsdoc',
-      'bower',
       'copy',
       'less',
       // NOTE: the requirejs task is actually
@@ -145,9 +136,6 @@ module.exports = function(grunt) {
     // 'connect',
     'watch'
   ]);
-
-
-  grunt.registerTask('prepublish', ['build', 'changelog']);
 
   grunt.registerTask('default', ['build']);
 };
