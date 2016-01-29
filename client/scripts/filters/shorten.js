8382c3b3584e821ec7561850f07282836454792e
Sebastian Stamm <sebastian.stamm@camunda.com>
2016-01-29 11:41:38 +0100

    chore(project): move development to camunda-bpm-webapp
    related to CAM-5298

--- client/scripts/filters/shorten.js
+++
@@ -1,7 +0,0 @@
-  'use strict';
-  var ShortenFilter = function() {
-    return function(input, length) {
-      return input.length > length ? input.substring(0, length) + '...' : input;
-    };
-  };
-  module.exports = ShortenFilter;
--- client/scripts/resources/localExecutionVariableResource.js
+++
@@ -1,9 +0,0 @@
-  'use strict';
-
-  var Resource = [ '$resource', 'Uri', function ($resource, Uri) {
-
-    return $resource(Uri.appUri('engine://engine/:engine/execution/:executionId/localVariables/:localVariableName'), { }, {
-      updateVariables : {method: 'POST'}
-    });
-  }];
-  module.exports = Resource;
