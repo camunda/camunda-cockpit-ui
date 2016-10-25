00a6795b701e6d0e88b776750fc2f3d3f72bf676
Askar Akhmerov <askar.akhmerov@camunda.com>
2016-10-25 12:11:37 +0200

    chor(cockpit): allow hyphen in engine name
    related to #CAM-3500

--- client/scripts/camunda-cockpit-ui.js
+++ client/scripts/camunda-cockpit-ui.js
@@ -74,7 +74,7 @@ define('camunda-cockpit-ui', [
       UriProvider.replace(':engine', [ '$window', function($window) {
         var uri = $window.location.href;
 
-        var match = uri.match(/\/app\/cockpit\/(\w+)(|\/)/);
+        var match = uri.match(/\/app\/cockpit\/([\w-]+)(|\/)/);
         if (match) {
           return match[1];
         } else {
