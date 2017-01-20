eb5a1670e98964e77d3dc70ce029d7a3aec262c3
Roman Smirnov <roman.smirnov@camunda.com>
2017-01-20 11:08:07 +0100

    chore(cockpit): set unsafe parameter on message
    related to CAM-6242

--- client/scripts/repository/deployments/directives/cam-cockpit-deployments.js
+++ client/scripts/repository/deployments/directives/cam-cockpit-deployments.js
@@ -48,11 +48,12 @@ define([
         // control ///////////////////////////////////////////////////////////////////
 
         var control = $scope.control = {};
-        control.addMessage = function (status, msg) {
+        control.addMessage = function (status, msg, unsafe) {
           Notifications.addMessage({
             status: status,
             message: msg,
-            scope: $scope
+            scope: $scope,
+            unsafe: unsafe
           });
         };
 
